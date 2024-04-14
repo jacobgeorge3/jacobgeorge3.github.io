a63df4b9651b349fcea68b8dda91eadefc065a55
Jacob George <jacob.c.george333@gmail.com>
2024-04-14 17:36:49 -0500

    Populating table using buttons

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-function blogNav(button){
+function blogNav(el){
     // Deactivate all buttons with the class "myButton"
-    var buttons = document.querySelectorAll('.blogButtons');
-    buttons.forEach(function(btn) {
-        btn.disabled = true;
-    });
+    const items1 = [
+        { date: "10/17/2018", title: "climbing1", summary:"summary1" },
+        { date: "10/18/2018", title: "climbing2", summary:"summary2" },
+      ];
+      const items2 = [
+        { date: "10/17/2019", title: "book1", summary:"summary1" },
+        { date: "10/18/2019", title: "books2", summary:"summary2" },
+      ];
 
-    // Activate the clicked button
-    button.disabled = false;
+    var button = el
+    console.log(el)
+    
+    let comp = el.id.localeCompare("climbingBlogButton");
+    if(comp == 0){
+        console.log("climbing button")
+        loadTableData(items1);
+        loadTableData([]);
+    }
+
+    comp = el.id.localeCompare("booksBlogButton");
+    if(comp == 0){
+        console.log("books button")
+        loadTableData(items2);
+        loadTableData([]);
+    }
+
+      
 }
         
+
+
+function loadTableData(items) {
+
+    const table = document.getElementById("testBody");
+    items.forEach( item => {
+      let row = table.insertRow();
+      let date = row.insertCell(0);
+      date.innerHTML = item.date;
+      let title = row.insertCell(1);
+      title.innerHTML = item.title;
+      let summary = row.insertCell(2);
+      summary.innerHTML = item.summary;
+    });
+  
+  }
